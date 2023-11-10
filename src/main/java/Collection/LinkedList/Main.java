96f9110f011bcbb887fe941f54f4e1b7492f7e0e
DS-Gordeev <gordeyka@list.ru>
2023-11-10 18:23:33 +0300

    add LinkedList lesson

--- src/main/java/Collection/LinkedList/Main.java
+++ src/main/java/Collection/LinkedList/Main.java
@@ -1,4 +1,25 @@
 package Collection.LinkedList;
 
+import java.util.LinkedList;
+
 public class Main {
+    public static void main(String[] args) {
+        LinkedList<Integer> intLinkedList = new LinkedList<>();
+        intLinkedList.add(1);
+        intLinkedList.add(2);
+        intLinkedList.add(3);
+        intLinkedList.add(4);
+        intLinkedList.add(5);
+        System.out.println(intLinkedList);
+
+        // Операции на получения элемента работают медленне чем в ArrayList
+        // Т.к. элемент содержит только ссылки на пред. и посл. и нужно по ссылкам идти до искомого
+        // HEAD перед собой и TAIL после себя ссылаются на null
+        System.out.println(intLinkedList.get(4));
+
+        // Операции добавления .add и удаления .remove работают быстрее т.к. не нужно сдвигать весь массив
+        // происходит простая перелинковка соседних элементов
+        intLinkedList.add(3, 99);
+        System.out.println(intLinkedList);
+    }
 }
