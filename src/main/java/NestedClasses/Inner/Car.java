9f1393f0afc33e95e87751dde22cd77bb201b6a5
DS-Gordeev <gordeyka@list.ru>
2023-12-17 22:53:24 +0300

    add InnerClass lesson

--- src/main/java/NestedClasses/Inner/Car.java
+++ src/main/java/NestedClasses/Inner/Car.java
@@ -1,4 +1,42 @@
 package NestedClasses.Inner;
 
 public class Car {
+    String color;
+    int doorCount;
+    //По-умолчанию мощьность двигателя будет 0
+    Engine engine = this.new Engine(0);
+
+
+    public Car(String color, int doorCount) {
+        this.color = color;
+        this.doorCount = doorCount;
+    }
+    
+    public void setEngine(Engine engine) {
+        this.engine = engine;
+    }
+    @Override
+    public String toString() {
+        return "My car: {" +
+                "color='" + color + '\'' +
+                ", doorCount=" + doorCount +
+                ", engine=" + engine +
+                '}';
+    }
+
+    // Вложенный inner класс (обычный не static)
+    public class Engine {
+      private int HorsePower;
+
+        public Engine(int horsePower) {
+            this.HorsePower = horsePower;
+        }
+
+        @Override
+        public String toString() {
+            return "My Engine: {" +
+                    "HorsePower=" + HorsePower +
+                    '}';
+        }
+    }
 }
--- src/main/java/NestedClasses/Inner/Main.java
+++ src/main/java/NestedClasses/Inner/Main.java
@@ -1,4 +1,22 @@
 package NestedClasses.Inner;
 
 public class Main {
+    public static void main(String[] args) {
+
+        // Создаем объект myCar с двигателем по-умолчанию
+        Car myCar = new Car("black", 4);
+        System.out.println(myCar);
+
+        // Создаем двигатель, т.к. класс Engine не Static, то для создания необходим уже созданный объект myCar
+        Car.Engine myEngine = myCar.new Engine(150);
+        System.out.println(myEngine);
+
+        // При помощи setter устанавливаем двигатель в объект myCar
+        myCar.setEngine(myEngine);
+        System.out.println(myCar);
+
+
+
+
+    }
 }
